27c81e691bf945885f0157024a83521a2c5e02dc
ines5388 <142507801+ines5388@users.noreply.github.com>
2024-02-12 12:52:59 +0000

    Generador de Excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let laExcusa = () => {
+    let tipoDePasta = ["Gnoquis", "Sorrentinos", "Canelones"];
+    let tipoDeSalsa = [
+      "bolognesa",
+      "carbonara",
+      "rosa",
+      "de crema y champignon",
+      "mixta"
+    ];
+    let quien = ["mi mamá", "mi abuela", "mi hermano", "mi sobrina"];
+
+    let tipoDePastaIndice = Math.floor(Math.random() * tipoDePasta.length);
+    let tipoDeSalsaIndice = Math.floor(Math.random() * tipoDeSalsa.length);
+    let quienIndice = Math.floor(Math.random() * quien.length);
+
+    return (
+      tipoDePasta[tipoDePastaIndice] +
+      " con salsa " +
+      tipoDeSalsa[tipoDeSalsaIndice] +
+      " hecha por " +
+      quien[quienIndice]
+    );
+  };
+  document.getElementById("excuse").innerHTML = laExcusa();
 };
